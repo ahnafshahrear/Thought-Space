052a19e8600797da3195ca706fbc2ca6abdef444
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-11-12 17:38:18 +0600

    Blog show page updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,7 @@
 <x-layout>
-    <div class="card shadow-lg p-3 mb-3">
-        <div class="card-body">
-            <h3 class="card-title">{{ $post->title }}</h3>
-            <h6 class="card-subtitle mb-2 text-body-secondary">Posted {{ $post->created_at->diffForHumans() }} by <a href="{{ route('posts.user', $post->user)  }}">{{ $post->user->username }}</a></h6>
-            <p class="card-text"><span>{{ $post->body }}</span></p>
-        </div>
+    <div class="">
+        <h3 class="card-title fw-bold fs-2 mb-2">{{ $post->title }}</h3>
+        <h6 class="card-subtitle mb-4 text-body-secondary">Posted {{ $post->created_at->diffForHumans() }} by <a href="{{ route('posts.user', $post->user)  }}">{{ $post->user->username }}</a></h6>
+        <p class="card-text fw-normal fs-5">{{ $post->body }}</p>
     </div>
 </x-layout>
